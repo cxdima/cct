fa8946bcf58088f42630909354aacff7e6cac656
Dmitry Moiseenko <cxdimafeed@gmail.com>
2025-05-29 12:49:01 -0500

    removed unnecessary s3 policy

--- s3.tf
+++ s3.tf
@@ -8,16 +8,3 @@ resource "aws_s3_bucket_website_configuration" "site" {
     suffix = "index.html"
   }
 }
-
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.site.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = ["s3:GetObject"],
-      Resource  = "${aws_s3_bucket.site.arn}/*"
-    }]
-  })
-}
