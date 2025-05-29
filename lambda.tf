e08392adc157d7749ff6348a14f5f3b6f98de51b
Dmitry Moiseenko <cxdimafeed@gmail.com>
2025-05-29 10:28:34 -0500

    Changed terraform lambda package

--- lambda.tf
+++ lambda.tf
@@ -19,10 +19,10 @@ resource "aws_iam_policy_attachment" "lambda_basic_execution" {
 resource "aws_lambda_function" "handler" {
   function_name    = "${var.project_name}-lambda"
   runtime          = "python3.11"
-  handler          = "index.handler"
+  handler          = "ambda_function.lambda_handler"
   role             = aws_iam_role.lambda_exec_role.arn
-  filename         = "lambda_function_payload.zip" # Upload before applying
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  filename         = "function.zip" # Upload before applying
+  source_code_hash = filebase64sha256("function.zip")
 }
 
 resource "aws_apigatewayv2_api" "api" {
@@ -49,3 +49,12 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
